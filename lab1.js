d0ef7110c981e703ee9a0d399a43c81ea9df9f1e
Daniels <G00381830@atu.ie>
2023-09-30 12:25:48 +0000

    added comments

--- lab1.js
+++ lab1.js
@@ -1,15 +1,18 @@
+// 2 a), b) arrow function to return a variable
 const part1 = (string)=>{
     console.log(string);
 };
 
 part1("Data Representation & Querying");
 
+// 2 c) arrow function to add 2 numbers
 const addition = (num1, num2)=>{
     console.log(num1 + num2);
 };
 
 addition(2, 6);
 
+// 2 d) multiply numbers from an array function and use .maps
 const ages = [25, 31, 42, 77];
 
 const age = (num)=>{
@@ -23,8 +26,10 @@ const age = (num)=>{
 
 console.log(ages.map(age));
 
+// 3 a) create string array
 const strings = [];
 
+// 3 b) addTask function to add elements to array
 const addTask = (task)=>{
     strings.push(task);
     console.log("added " + task + " to array");
@@ -35,6 +40,7 @@ addTask("cheese");
 addTask("yellow");
 addTask("11");
 
+// 3 c) list tasks through a loop
 const listAllTasks = ()=>{
     for (var i = 0; i < strings.length; i++)
     {
@@ -44,6 +50,7 @@ const listAllTasks = ()=>{
 
 listAllTasks();
 
+//3 d) deleteTask function 
 const deleteTask = (task)=>{
     var index = strings.indexOf(task);
     if (index !== -1) {
